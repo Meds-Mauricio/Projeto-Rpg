dcab91fcbc6464ca8e923e02608ada8ce23a0a55
Mauricio Edson de Souza <mauricioesdonsouza@gmail.com>
2022-07-11 17:56:06 -0300

    processo-de-finalizar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,45 @@
+import { useEffect } from 'react';
 import * as C from './App.styles';
+import { Character } from "./components/character";
+import { useCharacter } from './hooks/useCharacter';
 
 const App = () => {
+    const char = useCharacter();
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+    }, []);
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        switch (e.code) {
+            case 'KeyA':
+            case "ArrowLeft":
+                char.moveLeft();
+
+                break;
+            case 'KeyW':
+            case 'ArrowUp':
+                char.moveUp();
+
+                break;
+            case 'KeyD':
+            case 'ArrowRight':
+                char.moveRight();
+
+                break;
+            case 'KeyS':
+            case 'ArrowDown':
+                char.moveDown();
+        }
+    }
+
     return (
         <C.Container>
             <C.Map>
-                
+                <Character x={char.x} y={char.y}  side={char.side}/>
             </C.Map>
         </C.Container>
-    )
+    );
 }
 
 export default App;
--- my-app/src/components/character/index.tsx
+++ my-app/src/components/character/index.tsx
@@ -1,9 +1,29 @@
 import * as C from './styles';
+import { CharacterSides } from '../../types/characterSides';
 
-export const character = () => {
+type Props = {
+    x: number;
+    y: number;
+    side: CharacterSides;
+}
+
+
+export const Character = ({ x, y, side }: Props) => {
+    const size = 30;
+    const sides = {
+        down: 0,
+        left: -30,
+        right:-60,
+        up: -90
+    }
+
     return (
-        <C.Container>
-            ...
+        <C.Container
+            size={size}
+            left={x * size}
+            top={y * size}
+            sidePos={sides[side] ?? 0} 
+        >
         </C.Container>
     )
 }
